3d069e38200f09858be78d94a17c7f8bbb685750
xxholly32 <xxholly32@163.com>
2020-10-28 17:46:28 +0800

    feat: add github icon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import vitePluginVuedoc from 'vite-plugin-vuedoc'
 
 const config = {
-  base: "/vite-components-playground/",
+  publicPath : "/vite-components-playground/",
   // can read default _assets at Github pages
   assetsDir: "assets",
   plugins: [vitePluginVuedoc()]
