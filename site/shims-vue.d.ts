60e32ab3b840b336ee9e255f7b9aa3f8f2cd21ee
xxholly32 <xxholly32@163.com>
2020-11-02 18:14:11 +0800

    fix: change site from src to site

--- site/shims-vue.d.ts
+++ site/shims-vue.d.ts
@@ -3,3 +3,9 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module "*.md" {
+  import { ComponentOptions } from "vue";
+  const comp: ComponentOptions;
+  export default comp;
+}
