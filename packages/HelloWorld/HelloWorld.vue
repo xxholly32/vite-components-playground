892d4ab450d667abe1c2f23bc008d97e22866b50
xxholly32 <xxholly32@163.com>
2020-11-05 17:51:36 +0800

    docs: change doc bug

--- packages/HelloWorld/HelloWorld.vue
+++ packages/HelloWorld/HelloWorld.vue
@@ -3,14 +3,14 @@
     <h1>hello {{ msg }}</h1>
     <button @click="count++">count is: {{ count }}</button>
     <p>
-      Edit <code>components/HelloWorld.vue</code> to test hot module
+      Edit <code>packages/HelloWorld/HelloWorld.vue</code> to test hot module
       replacement.
     </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "HelloWorld",
   props: {
