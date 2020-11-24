6650e5283058851cf1037eea1ba84b27f57234ec
xxholly32 <xxholly32@163.com>
2020-11-24 17:34:33 +0800

    feat: build components with tailwind

---
+++ packages/Button/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn" :class="[type ? 'btn-' + type : '']">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "el-button",
+  props: {
+    type: String,
+  },
+});
+</script>
+<style lang="css">
+@import url("./style/main.css");
+</style>
